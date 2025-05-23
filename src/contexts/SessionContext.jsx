f9d8595cd7531af4cf88c63bf90fc5384d82c429
IsaacAlbiston <isaacalbiston@gmail.com>
2025-05-23 16:42:57 +0100

    hide account option when not logged in

--- src/contexts/SessionContext.jsx
+++ src/contexts/SessionContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const SessionContext = createContext()
 
 export const SessionProvider = ({children})=>{
-    const [username, setUsername] = useState("tickle122")
+    const [username, setUsername] = useState("")
 
     return <SessionContext.Provider value={{username, setUsername}}>
         {children}
